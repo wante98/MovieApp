0933be89cd7b2dd368d21d874de56eb558f796b8
wante98@gmail.com <wante98@gmail.com>
2022-06-14 23:22:32 +0800

    Feat: Fix the bug that website cannot be deplay

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false //若有Vue项目报错： Component name “xxx“ should always be multi-word vue/multi-word-component-names就硬加上這個
+  lintOnSave: false, //若有Vue项目报错： Component name “xxx“ should always be multi-word vue/multi-word-component-names就硬加上這個
+  //由於 github-pages 並非在網頁根目錄顯示成果 , 所以需要增加 vue.config.js 調整網頁根目錄
+  publicPath: '/MovieApp/'
 })  
